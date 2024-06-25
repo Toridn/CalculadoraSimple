3644c72e5fdb3172772df2dd27489fd7e65c3a3b
Toridn <134969625+Toridn@users.noreply.github.com>
2024-06-25 08:58:39 -0400

    Add files via upload
    Se creo una validación para que la división no sea por 0
    se pregunta si desea dividir el primer numero por el segundo o el segundo por el primero

--- Calculador.py
+++ Calculador.py
@@ -6,19 +6,38 @@ def Suma(num1, num2):
     return res
 #Funcion que rest los valores
 def Resta(num1, num2):
-    res=num1-num2
+    print("1.- Numero 1 -Numero 2")
+    print("2.- Numero 2 - Numero 1")
+    op=int(input("Ingres el orden en que desea realizarla operacion"))
+
+    if op==1:
+        res=num1-num2
+    else:
+        res=num2-num1
     return res
+
 #Funcion que multilica los valores
 def Multiplicacion(num1,num2):
     res=num1*num2
     return res
 #Funcion que divide los valores
 def Dividir(num1, num2):
-    if num2!=0:
-        res= num1/num2
-        return res
+    print("1.- Numero 1 / Numero 2")
+    print("2.- Numero 2 / Numero 1")
+    op= int(input("Ingrese el orden en que desea realizar la operacion"))
+
+    if op==1:
+        if num2!=0:
+            res= num1/num2
+            return res
+        else:
+            print("No se puede dividir por 0")
     else:
-        print("No se puede dividir por 0")
+        if num1!=0:
+            res=num2/num1
+            return res
+        else:
+            print("no se puede dividir por 0")
 
 
 
@@ -33,8 +52,16 @@ while True:
     op=int(input("Ingrese el numero de la operacion que desea realizar: "))
 
     if op==1 or op==2 or op==3 or op==4 :
-        Numero1 = int(input("Ingrese el primer numero: "))
-        Numero2 = int(input("Ingrese el segundo numero: "))
+        while True:
+            Numero1 = int(input("Ingrese el primer numero: "))
+            Numero2 = int(input("Ingrese el segundo numero: "))
+
+            if not (op==4 and Numero1==0 or Numero2==0 ):
+                
+                break
+            else:
+                print("Para la la division ninguno de los numeros puede ser 0, intentelo otra vez")
+         
 
     if op==1:
         #resultado = Suma(Numero1,Numero2)
